3997ed7e7738d4bd83702ab805909bf7d0a0fb1a
Jovan Lanik <jox969@gmail.com>
2022-05-20 01:38:08 +0200

    fix #5

--- window.c
+++ window.c
@@ -171,8 +171,8 @@ static void window_setup(struct Window *ctx) {
 			gtk_widget_set_size_request(ctx->body, 384, -1);
 			gtk_container_add(GTK_CONTAINER(ctx->window_box), ctx->body);
 			window_update_clock(ctx);
+			window_setup_input(ctx);
 		}
-		window_setup_input(ctx);
 	} else if(ctx->body != NULL) {
 		gtk_widget_destroy(ctx->body);
 		ctx->body = NULL;
