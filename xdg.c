87f9f1c9deb512f6148989dbc1377f50797edc0e
Jovan Lanik <jox969@gmail.com>
2022-06-14 16:47:35 +0200

    reimplement xdg to use glib functions

--- xdg.c
+++ xdg.c
@@ -1,75 +1,28 @@
-#define _POSIX_C_SOURCE 200809L
+#include "glib.h"
 
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <linux/limits.h>
-#include <sys/stat.h>
-
-#define HOME_STYLE_SUFFIX "/.config/gtklock/style.css"
-#define ETC_STYLE_PATH "/etc/gtklock/style.css"
-
-/*
- * resolve_xdg_style_path_home will perform a lookup of the current user's data
- * to discover their home directory. 
- *
- * Once the home discovery is discovered, a lookup of '~/.config/gtklock/style.css'
- * is performed. 
- *
- * If discovered the fully resolved path is returned, if not nil is returned.
- *
- * The caller should free the returned pointer when there is no longer use for it.
- */
-char *resolve_xdg_style_path_home(void) {
-	char *style_path = malloc(PATH_MAX);
-
-	uid_t euid = geteuid();
-	struct passwd *pwd = getpwuid(euid);
-	if(!pwd) goto null;
-	strcpy(style_path, pwd->pw_dir);
-	strcat(style_path, HOME_STYLE_SUFFIX);
-
-	struct stat s;
-	if(stat(style_path, &s) == -1) goto null;
-
-	if(S_ISREG(s.st_mode) || S_ISLNK(s.st_mode)) return style_path;
-
-null:
-	free(style_path);
+static char *resolve_xdg_style_path_home(void) {
+	const char *dir = g_get_user_config_dir();
+	char *path = g_build_path("/", dir, "gtklock", "style.css", NULL);
+	if(g_file_test(path, G_FILE_TEST_IS_REGULAR) == TRUE) return path;
 	return NULL;
 };
 
-/*
- * resolve_xdg_style_path_etc will perform a lookup of "style.css" in the canonical
- * "/etc/gtklock" directory.
- *
- * If discovered the fully resolved path is returned, if not nil is returned.
- *
- * The caller should free the returned pointer when there is no longer use for it.
- */
-char *resolve_xdg_style_path_etc(void) {
-	// We could pass back just the global ETC_STYLE_PATH but lets do this so
-	// the usage of free is consistent with resolve_xdg_style_path_home.
-	char *style_path = strdup(ETC_STYLE_PATH);
-
-	struct stat s;
-	if(stat(style_path, &s) == -1) goto null;
-	if(S_ISREG(s.st_mode) || S_ISLNK(s.st_mode)) return style_path;
-
-null:
-	free(style_path);
+static char *resolve_xdg_style_path_etc(void) {
+	const char * const *dir = g_get_system_config_dirs();
+	for(int i = 0; dir[i] != NULL; ++i) {
+		char *path = g_build_path("/", dir[i], "gtklock", "style.css", NULL);
+		if(g_file_test(path, G_FILE_TEST_IS_REGULAR) == TRUE) return path;
+	}
 	return NULL;
 };
 
 char *resolve_xdg_style_path(void) {
-	char *style_path;
+	char *style_path = NULL;
 	// prefer user directory
 	style_path = resolve_xdg_style_path_home();
 	if(style_path) return style_path;
 	// then etc directory
-	style_path = resolve_xdg_style_path_home();
+	style_path = resolve_xdg_style_path_etc();
 	if(style_path) return style_path;
 	return NULL;
 }
