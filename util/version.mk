2690d6ba59ba9ab4e4aa9219b2cd4e621518522f
Jovan Lanik <jox969@gmail.com>
2022-09-09 01:31:16 +0200

    update version.mk

--- util/version.mk
+++ util/version.mk
@@ -3,24 +3,14 @@
 
 # Version
 
-VERSION := v1.3.5
+VERSION := v1.3.6
 command := $(shell command -v git)
 ifdef command
 	git := $(shell git rev-parse --is-inside-work-tree 2>&1 >/dev/null)
 	ifeq '$(.SHELLSTATUS)' '0'
-		tag := $(shell git describe --tags --exact-match 2>/dev/null)
-		ifeq '$(.SHELLSTATUS)' '0'
-			ifneq '$(tag)' '$(VERSION)'
-				sed := $(shell sed -i 's/^VERSION := $(VERSION)/VERSION := $(tag)/g' '$(lastword $(MAKEFILE_LIST))')
-			endif
-		else
-			tag := $(shell git describe --tags --always)
-		endif
-		VERSION := $(tag)
+		VERSION := $(shell git describe --tags --always)
 	endif
 endif
 
 undefine command
 undefine git
-undefine tag
-undefine sed
